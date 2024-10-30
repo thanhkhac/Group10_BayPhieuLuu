9f948bea9bcce85f5dc42b45e0a542c372dcbd92
Khac Thanh <thanhcqb2048@gmail.com>
2024-10-30 15:13:25 +0700

    Merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,76 +23,67 @@ public class PlayerMovement : MonoBehaviour
     {
         UpdateAnimationStates();
         horizontal = Input.GetAxis("Horizontal");
-
         Move();
-        
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            Jump();
-        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded()) { Jump(); }
     }
 
     private void Move()
     {
         if (canMove)
         {
-            rb.velocity = new Vector2(horizontal * speed , rb.velocity.y);
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
             if (horizontal > 0)
                 transform.localScale = new Vector3(1, 1, 1);
             else if (horizontal < 0)
                 transform.localScale = new Vector3(-1, 1, 1);
-        }else
-        {
-            rb.velocity = new Vector2(horizontal * 0 , rb.velocity.y);
         }
-        
-
+        else { rb.velocity = new Vector2(horizontal * 0, rb.velocity.y); }
     }
 
     private void UpdateAnimationStates()
     {
         animator.SetBool("isRunning", Mathf.Abs(horizontal) > 0.1f);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //Chuột trái
         {
             animator.SetBool("isAttacking", true);
-        }else
-        {
-            animator.SetBool("isAttacking", false);
         }
+        else { animator.SetBool("isAttacking", false); }
 
-        if (Input.GetKeyDown(KeyCode.Mouse2)) // Assume Fire2 is Range Attack
+        if (Input.GetKeyDown(KeyCode.Mouse2)) //Chuột giữa
         {
             animator.SetBool("isRangeAttacking", true);
         }
-        else
-        {
-            animator.SetBool("isRangeAttacking", false);
-        }
-
+        else { animator.SetBool("isRangeAttacking", false); }
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
-        isJumping = true; 
+        isJumping = true;
     }
 
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-    
-    public void EndDisableMove ()
+    private bool IsGrounded() => Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+    public void EndDisableMove()
     {
         Debug.Log("EndDisableMove");
         canMove = true;
     }
-    
-    public void DisableMove ()
+
+    public void DisableMove()
     {
         Debug.Log("DisableMove");
         canMove = false;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyAttack"))
+        {
+            animator.SetTrigger("Hit");
+        }
     }
 }
