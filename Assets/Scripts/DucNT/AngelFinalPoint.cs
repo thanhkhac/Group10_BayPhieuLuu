698e8a2b78957cf2a42cb524400ee8ebb2138afe
DuucNguyen <ducnthe173064@fpt.edu.vn>
2024-11-06 09:02:36 +0700

    Update Final Point

--- Assets/Scripts/DucNT/AngelFinalPoint.cs
+++ Assets/Scripts/DucNT/AngelFinalPoint.cs
@@ -21,6 +21,7 @@ public class AngelFinalPoint : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            Debug.Log("HitPlayer");
             LoadScene();
         }
     }
