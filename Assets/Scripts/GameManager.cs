ca6e6f2b5d4e0033197c9e2c2c63123b383306c6
Khac Thanh <thanhcqb2048@gmail.com>
2024-11-06 08:37:43 +0700

    Update Game2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,6 +35,7 @@ namespace DefaultNamespace
         // Hàm New Game, có thể là khởi đầu lại từ đầu
         public void NewGame()
         {
+            PlayerData.CurrentLevel = 1;
             ContinueGame();
             PlayerData.PLayerHealth = 100;
             PlayerData.PLayerMana = 100;
